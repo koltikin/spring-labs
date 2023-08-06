b3e7942ce106616ad53be1f8767a073ae9e41235
ziya <koltikin210@gmail.com>
2023-08-06 12:11:31 +0300

    Spring lab-02 DI is done

--- lab-02-di/src/main/java/com/cydeo/service/CartService.java
+++ lab-02-di/src/main/java/com/cydeo/service/CartService.java
@@ -2,7 +2,9 @@ package com.cydeo.service;
 
 import com.cydeo.model.Cart;
 import com.cydeo.model.Product;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface CartService {
     Cart addCart(Product product, int quantity);
 }
