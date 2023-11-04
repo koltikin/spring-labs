422a2e1ae95a82570485151a524300d1656a7bf8
ziya <koltikin210@gmail.com>
2023-11-04 23:29:57 +0300

    CategoryRepository is done

--- lab-07-orm-queries/src/main/java/com/cydeo/repository/CategoryRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/repository/CategoryRepository.java
@@ -5,8 +5,14 @@ import com.cydeo.Entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
     //Write a derived query to get category by name
+    Category findAllByName(String name);
+
     //Write a derived query to get top 3 categories order by name desc
+    List<Category> findTop3ByOrderByNameDesc();
+
 }
