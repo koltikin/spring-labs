a54fdb5ff981ab150614c35d7d9512034bb09297
ziya <koltikin210@gmail.com>
2023-11-04 23:33:36 +0300

    CustomerRepository is done

--- lab-07-orm-queries/src/main/java/com/cydeo/repository/CustomerRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/repository/CustomerRepository.java
@@ -3,11 +3,19 @@ package com.cydeo.repository;
 import com.cydeo.Entity.Address;
 import com.cydeo.Entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
     //Write a derived query to get all customer by id
+    Customer findAllById(Long id);
+
     //Write a JPQL query to get customer by email
+    @Query("SELECT c FROM Customer c WHERE c.email = ?1")
+    List<Customer> getAllByEmail(String email);
+
 
 }
