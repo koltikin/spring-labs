aa953e6d72dd062b7594175a6fde7583ec0b057f
ziya <koltikin210@gmail.com>
2023-11-26 23:49:14 +0300

    some method are added to GlobalExceptionHandler class

--- lab-09-rest-consume-Api/src/main/java/com/cydeo/exception/ExceptionWrapper.java
+++ lab-09-rest-consume-Api/src/main/java/com/cydeo/exception/ExceptionWrapper.java
@@ -1,19 +1,25 @@
 package com.cydeo.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExceptionWrapper {
     private String message;
     private HttpStatus httpStatus;
     private LocalDateTime localDateTime;
+
+    private Integer errorCount;
+    private List<ValidationException> validationExceptions;
 
     public ExceptionWrapper(String message, HttpStatus httpStatus) {
         this.message = message;
--- lab-09-rest-consume-Api/src/main/java/com/cydeo/exception/GlobalExceptionHandler.java
+++ lab-09-rest-consume-Api/src/main/java/com/cydeo/exception/GlobalExceptionHandler.java
@@ -2,22 +2,23 @@ package com.cydeo.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({Exception.class, RuntimeException.class, Throwable.class})
-    public ResponseEntity<ExceptionWrapper> handleGenericException(){
+    public ResponseEntity<ExceptionWrapper> handleGenericException(Throwable exception){
+        exception.printStackTrace();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ExceptionWrapper.builder()
-                        .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .message("Action failed: An error occurred")
-                        .localDateTime(LocalDateTime.now())
-                        .build());
+                .body(new ExceptionWrapper("Action failed: An error occurred",
+                        HttpStatus.INTERNAL_SERVER_ERROR));
     }
     @ExceptionHandler(CurrencyInvalidException.class)
     public ResponseEntity<ExceptionWrapper> handleCurrencyInvalidException(CurrencyInvalidException exception) {
@@ -28,6 +29,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(OrderNotFoundException.class)
     public ResponseEntity<ExceptionWrapper> handleOrderNotFoundException(OrderNotFoundException exception) {
         ExceptionWrapper exceptionWrapper = new ExceptionWrapper(exception.getMessage(), HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionWrapper);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionWrapper> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        exception.printStackTrace();
+
+        ExceptionWrapper exceptionWrapper = new ExceptionWrapper("Invalid Input(s)", HttpStatus.BAD_REQUEST);
+        List<ValidationException> exceptions = new ArrayList<>();
+        for (ObjectError error : exception.getFieldErrors())
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionWrapper);
     }
 }
